52670ab0a5ad1183eea4f829ad8420d48a4841c4
YamaHT <136429919+YamaHT@users.noreply.github.com>
2024-10-18 03:15:17 +0700

    Update profile.js

--- src/main/webapp/js/profile.js
+++ src/main/webapp/js/profile.js
@@ -1,8 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
-
 function onFileSelected(event) {
     var selectedFile = event.target.files[0];
     var reader = new FileReader();
